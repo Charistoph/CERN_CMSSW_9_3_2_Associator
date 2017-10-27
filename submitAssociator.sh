8c2527476e20355dfe1471bc95ea35d577865924
Christoph Bernkopf <christoph.bernkopf95@gmail.com>
2017-10-27 07:19:15 +0200

    added git ignore file

---
+++ submitAssociator.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#BSUB -q 1nh
+#
+echo "Application started"
+cd $WORKDIR
+#cd /tmp/cbernkop
+cd /afs/cern.ch/user/c/cbernkop/Associator/CMSSW_9_3_2/src/
+echo "`scramv1 runtime -sh`"
+eval `scramv1 runtime -sh`
+echo "`scram b`"
+eval `scram b`
+echo "1 complete: cmsenv"
+cd $WORKDIR
+cmsRun /afs/cern.ch/user/c/cbernkop/Associator/associator_run_Cfg.py
+#python /afs/cern.ch/user/c/cbernkop/Associator/print_associator_tracks.py
+echo "2 complete: cmsRun"
+#mkdir /afs/cern.ch/work/c/cbernkop/condor_output/assoc_output_new
+#echo "3 complete: mkdir"
+cp output_gsf_associator.root /afs/cern.ch/work/c/cbernkop/condor_output/assoc_output_new/output_gsf_associator.root
+echo "3 complete: cp output_gsf_associator.root"
+cp step3.root /afs/cern.ch/work/c/cbernkop/condor_output/assoc_output_new/step3.root
+echo "4 complete: cp step3.root"
+
